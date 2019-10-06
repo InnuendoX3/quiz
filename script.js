f4d8140f11c967fe2410f7b58ce461fc1c721aa7
Johan Garzon <johanprograma@hotmail.com>
2019-10-06 09:28:27 +0200

    Sketch 1 of getting/setting checkboxes on question

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ class Quiz {
    setUsername(username) {
       this.username = username;
    }
-
    getUsername() { //Needed?
       return this.username;
    }
@@ -29,7 +28,6 @@ class Quiz {
    setQuestionsChosen(qChosen){
       this.questionsChosen = qChosen;
    }
-
    getQuestionsChosen() { //Nedded?
       return this.questionsChosen;
    }
@@ -37,11 +35,15 @@ class Quiz {
    getCurrentQuestionNr() {
       return this.currentQuestionNr;
    }
+   setCurrentQuestionNr(newCurrent) {
+      this.currentQuestionNr = newCurrent;
+   }
 
    getQuestion() {
       return this.questions[this.currentQuestionNr].question;
    }
 
+   
    getAnswers() {
       return this.questions[this.currentQuestionNr].answers;
    }
@@ -57,6 +59,7 @@ class Question {
       this.answers = [];
       this.addAnswers(cAnswers);
       this.rightAnswered = false;  // testing if needed
+      this.checkboxes = []; // Save users checkbox answer per question (true/flase)
    }
 
    addAnswers(cAnswers) {
@@ -64,6 +67,21 @@ class Question {
          let a = new Answer(i.answer, i.isRight);
          this.answers.push(a);
       }
+   }
+
+   // Read users answers on checkboxes
+   setCheckboxesAnswers(userAnswers) {
+      
+   }
+
+   // Clear all HTML checkboxes to could reload the question's answers (this.checkboxes)
+   clearCheckboxes() {
+      // Aqui no.
+   }
+
+   // Send how user had answered the question
+   getCheckboxesAnswers() {
+      return checkboxes;
    }
 }
 
@@ -73,6 +91,22 @@ class Answer {
       this.isRight = cIsRight;
    }
 }
+
+/*class Checkboxes {
+   constructor() {
+      this.checkboxes = [];
+   }
+
+   makeDiv() {
+      let div = document.createElement("div");
+      let checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      let label = document.createElement("label");
+      /* aqui voy -- imprimirlos aqui sirve? 
+   }
+
+
+}*/
 
 // Paste here from Mocky!!!
 let fromJson = getJSON("http://www.mocky.io/v2/5d9647d233000003cd2f9028");
@@ -101,10 +135,11 @@ document.addEventListener('DOMContentLoaded', function () {
    let qButtonsDiv = document.getElementById("q-buttons");
    let qNumberH2 = document.getElementById("q-number");
    let questionHere = document.getElementById("question-here");
-   let labelA1 = document.getElementById("labelA1");
-   let labelA2 = document.getElementById("labelA2");
-   let labelA3 = document.getElementById("labelA3");
-   let labelA4 = document.getElementById("labelA4");
+   let labelA1 = document.getElementById("labelA1"); // Nedeed?
+   let labelA2 = document.getElementById("labelA2"); // Nedeed?
+   let labelA3 = document.getElementById("labelA3"); // Nedeed?
+   let labelA4 = document.getElementById("labelA4"); // Nedeed?
+
 
    // Variables
 
@@ -113,6 +148,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
    // Event listeners
    formulario.addEventListener("submit", startQuiz);
+
 
 
    function startQuiz(event) {
@@ -154,13 +190,30 @@ document.addEventListener('DOMContentLoaded', function () {
       
    }
 
-   // Create a button per question... to navigate
+   // Create a button per question... to navigate. Add event listener per button.
    function printNavButtons() {
       for (let i=0; i < theWholeQuiz.getQuestionsChosen(); i++) {
+         // Create button with text and ID
          var newButton = document.createElement("button");
          newButton.innerHTML = i + 1;
+         newButton.id = "button-answer" + i; // create an ID per button: butAns0...
          qButtonsDiv.appendChild(newButton);
+
+         // Add to the button an event listener
+         let temp = document.getElementById("button-answer" + i);
+         temp.addEventListener("click", function() { 
+            changeQuestion(temp.innerHTML - 1); // Send the "index" of the question 
+         });
       }
+
+      
+
+   }
+
+   // Display a new question/answers depend on navigation-button
+   function changeQuestion(i) {
+      theWholeQuiz.setCurrentQuestionNr(i);
+      printEverything();  // Here?
    }
 
    function printEverything() {
@@ -179,7 +232,7 @@ document.addEventListener('DOMContentLoaded', function () {
          count++;
       }
 
-
+      
    }
    
 
